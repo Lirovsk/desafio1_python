55973141973a9860c814ba1ae672eccc45e27a86
João Vítor Lira <16088480602@estudante.ufjf.br>
2025-01-25 19:39:24 -0300

    atualizando functions.py e criando o conceito de main.py

--- functions.py
+++ functions.py
@@ -66,10 +66,10 @@ class client:
         endereco = input("Digite o endereço do cliente: ")
         self.dados['endereço'] = endereco
         return
-    
+      
+      
 class bank:
     def __init__(self, cpf, lista_contas):
-        #tranformar isso em um dicionário
         self.dados = {
             'cpf': cpf,
             'AGENCIA': '0001',
@@ -78,11 +78,58 @@ class bank:
             'extrato': [],
             'lmt_d': 0
         }
+        
        
 
     def criar_n_conta(self, lista_contas):
         if len(lista_contas) == 0:
-            self.n_conta = 1
+            n_conta = 1
         else:
-            self.n_conta = lista_contas[-1].dados['n_conta'] + 1
-        return self.n_conta
+            n_conta = lista_contas[-1].dados['n_conta'] + 1       
+        return n_conta
+    
+
+class search:
+    def search_client(self, cpf, lista_clientes):
+        #search for a client in the list of clients by cpf
+        for i in range(len(lista_clientes)):
+            if lista_clientes[i].dados['cpf'] == cpf:
+                a = 1
+                return lista_clientes[i]
+                
+        if a != 1: 
+            print("Cliente não encontrado")
+            return
+    
+    def search_n_conta(self, n_conta, lista_contas):
+        #search for a bank account in the list of bank accounts by account number
+        for i in range(len(lista_contas)):
+            if lista_contas[i].dados['n_conta'] == n_conta:
+                return i
+            else:
+                print("Conta não encontrada")
+                return
+
+class adding:
+    def add_cliente(self, lista_clientes):
+        #add a new client to the list of clients
+        answer = input("Deseja adicionar um novo cliente?\n")
+        if answer == 'sim':
+            cliente = client()
+            lista_clientes.append(cliente)
+            print("Cliente adicionado com sucesso!")
+        return
+
+    def add_conta(self, lista_contas, lista_clientes):
+        #create a bank account to a client and add it to the list of bank accounts
+        cpf = input("Digite o número do cpf do cliente que deseja adicionar a conta")
+        conta = bank(cpf, lista_contas)
+        lista_contas.append(conta)
+        #adding the account number to the client's account list
+        n_conta = conta.dados['n_conta']
+        serc = search()
+        cliente = serc.search_client(cpf, lista_clientes)
+        if cliente == None:
+            cliente.dados['contas'].append(n_conta)
+        print("Conta adicionada com sucesso!")
+        return
