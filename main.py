1359e2ada77e275bd1012f2de4c4c6333ede1f8c
João Vítor Lira <16088480602@estudante.ufjf.br>
2025-01-25 19:42:41 -0300

    definindo o conceito do main.py

--- main.py
+++ main.py
@@ -1,42 +1,22 @@
 from time import *
 from os import *
 from functions import *
-MENU = '''
-Seja Bem vindo ao novo sistema do banco!!
-Selecione as opções abaixo conforme o que deseja utilizar hoje:
-        Saque[1]
-        Depósito[2]
-        Extrato[3]
-        Sair do sistema[4]
-'''
-MSG_SAQUE = '''Digite o valor desajado para o saque. 
-Caso queira retornar ao menu sem realizar o saque, digite 'sair'.
-'''
-MSG_DEPOSITO = """Digite o valor desejado para o depósito.
-"""
+
+add = adding()
+lista_contas = []
+list_clientes = []
 keep = True
 while keep:
-    lista_contas = []
-    lista_clientes = []
-    print("deseja adicionar um novo cliente?")
+    print("o que deseja fazer hoje no banco?")
+    print("1.add cliente, 2 criar contas")
     resp = input()
-    if resp == 'sim':
-        cliente = client()
-        lista_clientes.append(cliente)
-        print("Cliente adicionado com sucesso!")
-    else:
-        continue
-    resp2 = input("Deseja adicionar uma nova conta para o cliente?")
-    #Gtornar isso uma função para despoluir o código
-    if resp2 == 'sim':
-        cpf = input("Digite o número do cpf do cliente que deseja adicionar a conta")
-        conta = bank(cpf, lista_contas)
-        lista_contas.append(conta)
-        print("Conta adicionada com sucesso!")
-    print("para entrar emum conta digite primeiro seu cpf:")
-    cpf = str(input())
+    if resp == '1':
+        add.add_cliente(list_clientes)
+    elif resp == '2':
+        add.add_conta(lista_contas, list_clientes)
+    
+    for i in range(len(list_clientes)):
+        print(list_clientes[i].dados)
+    
     for i in range(len(lista_contas)):
-        if lista_clientes[i].dados['cpf'] == cpf:
-            print(lista_clientes[i].dados['contas'])
-
-            break
+        print(lista_contas[i].dados)
