12d8384935d2b34b9cb034c7a110290f8fa5e3fb
João Vítor Lira <16088480602@estudante.ufjf.br>
2025-02-12 11:00:29 -0300

    not oficial changes

--- TEXT.py
+++ TEXT.py
@@ -2,7 +2,29 @@ import os
 import time
 
 class TEXTO:
-    WELCOME = """ 
+    @classmethod
+    def __init__(cls):
+        cls.lenguage = input("""
+Choose you lenaguage:
+    1. English
+    2. Português
+    Type only the number: """)
+        
+    
+    @staticmethod
+    def lenguage_chose(variable):
+        if variable == '1':
+            return '1'
+        elif variable == '2':
+            return '2'
+        
+    @classmethod
+    def lenguage_chose(cls, variable):
+        cls.lenguage_chose(cls.lenguage)
+          
+    if lenguage_chose == '2':
+        
+        WELCOME = """ 
 /$$$$$$$                                                      /$$                 /$$       /$$                   
 | $$__  $$                                                    | $$                | $$      |__/                   
 | $$  \ $$  /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$         /$$$$$$$  /$$$$$$       | $$       /$$  /$$$$$$  /$$$$$$ 
@@ -12,23 +34,23 @@ class TEXTO:
 | $$$$$$$/|  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$/      |  $$$$$$$|  $$$$$$/      | $$$$$$$$| $$| $$     |  $$$$$$$
 |_______/  \_______/|__/  |__/ \_______/ \______/        \_______/ \______/       |________/|__/|__/      \_______/
                                                                                                                    """
-    MENU = """
+        MENU = """
 Bom dia, o que o Senhor(a) deseja fazer no banco hoje? (para sair digite 4)
 1. Adicionar cliente
 2. Criar conta
 3. Entrar no caixa eletrônico
 4. Sair
 """
-    ENTER_ATM = """Deseja entrar no caixa eletrônico?"""
-    ENTER_CPF = """Digite o número do CPF"""
-    ENTER_ACCOUNT = """Digite o número da conta"""
-    CLIENT_NOT_FOUND = """Cliente não encontrado"""
-    ACCOUNT_NOT_FOUND = """Conta não encontrada"""
-    INVALID_OPTION = """Opção inválida"""
-    GOING_BACK = "Voltando ao menu principal"
+        ENTER_ATM = """Deseja entrar no caixa eletrônico?"""
+        ENTER_CPF = """Digite o número do CPF"""
+        ENTER_ACCOUNT = """Digite o número da conta"""
+        CLIENT_NOT_FOUND = """Cliente não encontrado"""
+        ACCOUNT_NOT_FOUND = """Conta não encontrada"""
+        INVALID_OPTION = """Opção inválida"""
+        GOING_BACK = "Voltando ao menu principal"
     
-    class BANK:
-        MENU = """
+        class BANK:
+            MENU = """
 CAIXA ELETRÔNICO        
 O que deseja fazer Hoje? (para sair digite 4)
 1. Depositar
@@ -36,31 +58,31 @@ O que deseja fazer Hoje? (para sair digite 4)
 3. Ver extrato
 4. Sair
 """
-        DEPOSIT = """Digite o valor do depósito"""
-        WITHDRAW = """Digite o valor do saque"""
-        ABOVE_LIMIT = """Valor acima do limite"""
-        INVALID_VALUE = """Valor inválido"""
-        INSUFFICIENT_FUNDS = """Saldo insuficiente"""
-        WITHDRAW_SUCCESS = """Saque realizado com sucesso!"""
-        DEPOSIT_SUCCESS = """Depósito realizado com sucesso!"""
-        EXTRATO = """Extrato bancário: """
-        ENTER_CPF_FOR_ACCOUNT = """Digite o número do cpf do cliente que deseja adicionar a conta\n"""
-        ACCOUNT_ADDED = """Conta adicionada com sucesso!"""
-        PRESS_TO_CLEAR = """Pressione enter para apagar essa tela e voltar ao menu do banco"""
+            DEPOSIT = """Digite o valor do depósito"""
+            WITHDRAW = """Digite o valor do saque"""
+            ABOVE_LIMIT = """Valor acima do limite"""
+            INVALID_VALUE = """Valor inválido"""
+            INSUFFICIENT_FUNDS = """Saldo insuficiente"""
+            WITHDRAW_SUCCESS = """Saque realizado com sucesso!"""
+            DEPOSIT_SUCCESS = """Depósito realizado com sucesso!"""
+            EXTRATO = """Extrato bancário: """
+            ENTER_CPF_FOR_ACCOUNT = """Digite o número do cpf do cliente que deseja adicionar a conta\n"""
+            ACCOUNT_ADDED = """Conta adicionada com sucesso!"""
+            PRESS_TO_CLEAR = """Pressione enter para apagar essa tela e voltar ao menu do banco"""
 
-    class CLIENT:
-        ADD_NAME = """Digite o nome do cliente: """
-        ADD_BIRTH = """Digite a data de nascimento do cliente: """
-        ADD_CPF = """Digite o CPF do cliente: """
-        ADD_ADDRESS = """Digite o endereço do cliente: """
-        ADD_CLIENT = """Deseja adicionar um novo cliente?\n"""
-        CLIENT_ADDED = """Cliente adicionado com sucesso!
+        class CLIENT:
+            ADD_NAME = """Digite o nome do cliente: """
+            ADD_BIRTH = """Digite a data de nascimento do cliente: """
+            ADD_CPF = """Digite o CPF do cliente: """
+            ADD_ADDRESS = """Digite o endereço do cliente: """
+            ADD_CLIENT = """Deseja adicionar um novo cliente?\n"""
+            CLIENT_ADDED = """Cliente adicionado com sucesso!
         """
-        VERIFY_DATA = """Verifique abaixo se os dados do cliente estão corretos."""
-        INCORRECT_DATA_ = """\nAlgun dado esta incorreto?"""
-        INFO_TO_CORRECT = """Qual dado deseja corrigir? (Escreva-o assim como aparece acima)"""
-        INFO_CORRECTED = """\nDado corrigido com sucesso!\n"""
-        ANOTHER_TO_CORRECT = """\nDeseja corrigir mais algum dado?"""
+            VERIFY_DATA = """Verifique abaixo se os dados do cliente estão corretos."""
+            INCORRECT_DATA_ = """\nAlgun dado esta incorreto?"""
+            INFO_TO_CORRECT = """Qual dado deseja corrigir? (Escreva-o assim como aparece acima)"""
+            INFO_CORRECTED = """\nDado corrigido com sucesso!\n"""
+            ANOTHER_TO_CORRECT = """\nDeseja corrigir mais algum dado?"""
     
     class functions:
         def going_back():
--- teste.py
+++ teste.py
@@ -1,1 +1,6 @@
+from TEXT import *
 
+text = TEXTO()
+
+print(text.MENU)
+#print(text.BANK.DEPOSIT)
